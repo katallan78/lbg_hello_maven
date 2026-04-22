29e7844bbe7a486ea4f118f09557ed9b82f5ebb3
katallan <katrinaallan@yahoo.co.uk>
2026-04-22 10:34:41 +0100

    EDIT: version 1.7 hello release build

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println(sayHello());
         System.out.println(sayHelloToSomeone("Alice"));
+        System.out.println(sayHelloToSomeone("Mina"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Kat"));
         System.out.println(sayHelloToSomeone("Tina"));
